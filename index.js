399d635b3bb3929d30542a3d1270dbd14329f983
Marvin-Palma <marvinumg@hotmail.com>
2020-02-25 22:52:44 -0600

    Conexión MongoAtlas

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var port = 3900;
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/api_rest_inventario', {useUnifiedTopology: true, useNewUrlParser: true}).then(()=>{
+//mongoose.connect('mongodb://localhost:27017/api_rest_inventario', {useUnifiedTopology: true, useNewUrlParser: true}).then(()=>{
+mongoose.connect('mongodb+srv://Marvin-Palma:DesarrolloPassword@cluster0-enslb.mongodb.net/test?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true}).then(()=>{
     console.log("La conexión a la base de datos se ha realizado bien!!!");
-
     //Crear servidor y escuchar peticiones htpp
     app.listen(port, ()=>{
         console.log('Servidor corriendo en http://localhost:'+port);
